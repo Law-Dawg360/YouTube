52ac555afd31fb2989fcfdb220fa63460db447b5
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-03-08 08:52:16 -0500

    Update YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -1,5 +1,18 @@
 # prompt: make me code that takes the title, thumbnail, and date of a YouTube video
 
+#import os
+#name = 'my_name'
+#value = 'my_value'
+#with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    #print(f'{name}={value}', file=fh)
+
+
+#import os
+#name = 'my_name'
+#value = 'my_value'
+#with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+    print(f'{name}={value}', file=fh)
+
 !pip install pytube
 import pytube
 import re
@@ -19,3 +32,20 @@ title, thumbnail, date = get_video_info(url)
 print("Title:", title)
 print("Thumbnail:", thumbnail)
 print("Date:", date)
+set_multiline_output("Title: ", title)
+set_multiline_output("Thumbnail: ", thumbnail)
+set_multiline_output("Date: ", date)
+def set_multiline_output(name, value):
+    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+        delimiter = uuid.uuid1()
+        print(f'{name}<<{delimiter}', file=fh)
+        print(value, file=fh)
+        print(delimiter, file=fh)
+#with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
+  #print("Title:", title)
+  #print("Thumbnail:", thumbnail)
+  #print("Date:", date)
+  #print(f'{name}<<{delimiter}', file=fh)
+  #print(value, file=fh)
+  #print(delimiter, file=fh)
+  #print(f'{name}={value}', file=fh)
