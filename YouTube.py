215d570734dd1e8cdced8e09538cc00c7a65742a
Law-Dawg360 <107430641+Law-Dawg360@users.noreply.github.com>
2024-03-08 08:54:04 -0500

    Update YouTube.py

--- YouTube.py
+++ YouTube.py
@@ -11,7 +11,7 @@
 #name = 'my_name'
 #value = 'my_value'
 #with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={value}', file=fh)
+    #print(f'{name}={value}', file=fh)
 
 !pip install pytube
 import pytube
